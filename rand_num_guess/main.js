8be9273c86e4e3b88b159a536abb0b10eac5f361
krishna <kv.250307@gmail.com>
2026-06-08 15:29:42 +0530

    comp rand num

--- rand_num_guess/main.js
+++ rand_num_guess/main.js
@@ -3,6 +3,6 @@ var num = Math.round(Math.random()*100)
 var tried=0;
 console.log(num);
 document.getElementById("submit").onclick = function(){
-let inp = (document.getElementById("input").value == num)?(tried++&&(document.getElementById('verdict').textContent = ('You have solved this in',tried))&&(document.getElementById("submit").innerHTML='<div></div>')):(parseInt(document.getElementById("input").value,10) < num)?tried++&&(document.getElementById('verdict').textContent = ("Please enter a larger number"))&&(document.getElementById('verdict').textContent = ('You used ',tried)):(parseInt(document.getElementById("input").value,10) > num)?(tried++&&(document.getElementById('verdict').textContent = ("Please enter a smaller number"))):console.log("error");
+let inp = (document.getElementById("input").value == num)?(tried++&&(document.getElementById("final").innerHTML=`<p id='tries'>You have solved this in ${tried} tries</p>`)):(parseInt(document.getElementById("input").value,10) < num)?tried++&&(document.getElementById("verdict").textContent="PLease type are larger number")&&alert("Please enter a larger number"):(parseInt(document.getElementById("input").value,10) > num)?(tried++&&(document.getElementById("verdict").textContent="Please enter a smaller number")&&alert("Please enter a smaller number")):console.log("error");
 }
 
